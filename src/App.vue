309f289e6a34cdd7e0caae15bdd63019f7e97d88
aayzfatih <fatihayaz076@gmail.com>
2024-03-27 22:49:08 +0300

    create,delete and toggle fav operation added

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,46 @@
 <template>
-  <header>
-    <div class="container">
-      <img src="" />
+  <main>
+    <header>
+      <img src="./assets/pinia-logo.svg" alt="pinia logo" />
+      <h1>Pinia Tasks</h1>
+    </header>
+    <!-- New Task Form -->
+    <div class="new-task-form">
+      <TaskForm />
     </div>
-  </header>
+
+    <!-- filter -->
+    <nav class="filter">
+      <button @click="filter = 'all'">All task</button>
+      <button @click="filter = 'favs'">Fav task</button>
+    </nav>
+
+    <!-- task list -->
+    <div v-if="filter === 'all'" class="task-list">
+      <p>You have {{ taskStore.totalCount }} task left to do</p>
+      <div v-for="task in taskStore.tasks">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+    <div v-else-if="filter === 'favs'" class="task-list">
+      <p>You have {{ taskStore.favCount }} favs left to do</p>
+      <div v-for="task in taskStore.favs">
+        <TaskDetails :task="task" />
+      </div>
+    </div>
+
+  </main>
 </template>
 
-<script>
-export default {
+<script setup>
+import TaskDetails from './components/TaskDetails.vue';
+import TaskForm from './components/TaskForm.vue';
+import { useTaskStore } from './stores/TaskStore';
+import { ref } from 'vue'
+const taskStore = useTaskStore()
 
-}
+const filter = ref("all")
 </script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TaskDetails.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="task">
+    <h3>{{ task.title }}</h3>
+    <div class="icons">
+      <i class="material-icons" @click="taskStore.deleteTask(task.id)">delete</i>
+      <i class="material-icons" :class="task.isFav ? 'active' : null" @click="taskStore.toggleFav(task.id)">favorite</i>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useTaskStore } from '@/stores/TaskStore';
+
+const taskStore = useTaskStore()
+const props = defineProps({
+  task: {
+    type: Array,
+    required: true
+  }
+})
+</script>
